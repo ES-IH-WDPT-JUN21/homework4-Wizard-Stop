c443dc654ff8d9979149d06abaa550f1f904c864
Jose-AB <84471534+Jose-AB@users.noreply.github.com>
2021-10-30 16:18:06 +0100

    update opportunity service

--- opportunity-service/src/main/java/com/ironhack/opportunityservice/controller/impl/OpportunityControllerImpl.java
+++ opportunity-service/src/main/java/com/ironhack/opportunityservice/controller/impl/OpportunityControllerImpl.java
@@ -1,15 +1,17 @@
 package com.ironhack.opportunityservice.controller.impl;
 
+import com.ironhack.opportunityservice.controller.DTO.OpportunityDTO;
 import com.ironhack.opportunityservice.controller.interfaces.OpportunityController;
 import com.ironhack.opportunityservice.model.Opportunity;
 import com.ironhack.opportunityservice.repository.OpportunityRepository;
+import com.ironhack.opportunityservice.service.interfaces.OpportunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class OpportunityControllerImpl implements OpportunityController {
@@ -17,24 +19,25 @@ public class OpportunityControllerImpl implements OpportunityController {
     @Autowired
     private OpportunityRepository opportunityRepository;
 
+    @Autowired
+    private OpportunityService opportunityService;
+
     @GetMapping("/opportunity/all")
     @ResponseStatus(HttpStatus.OK)
     public List<Opportunity> getAll(){
-        return opportunityRepository.findAll();
+        return opportunityService.getAll();
     }
 
     @GetMapping("/opportunity/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Opportunity getById(@PathVariable(name = "id") Long opportunityId){
-        Optional<Opportunity> optionalOpportunity = opportunityRepository.findById(opportunityId);
-        return optionalOpportunity.isPresent() ? optionalOpportunity.get() : null;
+    public Opportunity getById(@PathVariable(name = "id") Long opportunityId) throws ResponseStatusException {
+        return opportunityService.getById(opportunityId);
     }
-
 
     @PostMapping("/opportunity/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public Opportunity add(@Valid @RequestBody Opportunity opportunity){
-        return opportunityRepository.save(opportunity);
+    public Opportunity add(@Valid @RequestBody OpportunityDTO opportunity){
+        return opportunityService.add(opportunity);
     }
 
 }
--- opportunity-service/src/main/java/com/ironhack/opportunityservice/service/interfaces/OpportunityService.java
+++ opportunity-service/src/main/java/com/ironhack/opportunityservice/service/interfaces/OpportunityService.java
@@ -1,13 +1,15 @@
-package com.ironhack.opportunityservice.controller.interfaces;
+package com.ironhack.opportunityservice.service.interfaces;
+
 
 import com.ironhack.opportunityservice.controller.DTO.OpportunityDTO;
 import com.ironhack.opportunityservice.model.Opportunity;
 
 import java.util.List;
 
-public interface OpportunityController {
+public interface OpportunityService {
 
     List<Opportunity> getAll();
     Opportunity getById(Long opportunityId);
     Opportunity add(OpportunityDTO opportunity);
+
 }
