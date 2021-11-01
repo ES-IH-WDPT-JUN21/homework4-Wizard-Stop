d8164d04bacd8bd8daad8b092f95f890aaaa9e31
almudena-ars <almudena.ars@gmail.com>
2021-11-01 19:02:23 +0100

    Cleaned salesrep-service, now only has necessary atributes

--- salesrep-service/src/main/java/com/ironhack/salesrep/exceptions/ExistentElementException.java
+++
@@ -1,8 +0,0 @@
-package com.ironhack.salesrep.exceptions;
-
-public class ExistentElementException extends Exception  {
-    public ExistentElementException(String message) {
-        super(message);
-    }
-}
-
--- salesrep-service/src/main/java/com/ironhack/salesrep/model/SalesRep.java
+++ salesrep-service/src/main/java/com/ironhack/salesrep/model/SalesRep.java
@@ -1,12 +1,6 @@
 package com.ironhack.salesrep.model;
 
-
-import com.ironhack.salesrep.supportiveClasses.Colors;
-
 import javax.persistence.*;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
 
 @Entity
 public class SalesRep {
@@ -17,117 +11,25 @@ public class SalesRep {
 
     private String name;
 
-    @OneToMany(mappedBy = "salesRep")
-    private List<Lead> leads;
-
-    @OneToMany(mappedBy = "salesRep")
-    private List<Opportunity> opportunities;
 
     // Constructor
 
     public SalesRep() {
     }
 
-    public SalesRep(String name) {
-        setName(name);
+    public Long getId() {
+        return id;
     }
-
-    //////////////////////////APPLICABLE METHODS/////////////////
-
-    public static String getNameFromUser(){
-        Scanner scanner = new Scanner(System.in);
-        String name = "";
-        try {
-
-            name = scanner.nextLine();
-        } catch (InputMismatchException e){
-            name= "";
-        }
-
-        return name;
-    }
-
-    public static Long getSalesRepIdFromCommand(){
-
-        Scanner scanner = new Scanner(System.in);
-        Long salesRepId = Long.valueOf(0);
-        try {
-            String lectorComando = scanner.nextLine();
-            salesRepId = Long.parseLong(lectorComando);
-        } catch (InputMismatchException e){
-            salesRepId = Long.valueOf(0);
-        }
-        catch (NumberFormatException e){
-            salesRepId = Long.valueOf(0);
-        }
-        if (salesRepId < 1) {
-            wrongIdSalesRep();
-            salesRepId = Long.valueOf(0);
-        }
-        return salesRepId;
-
-    }
-
-
-    ////////////////////////////////GETTERS AND SETTERS//////////////////////
-
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public static String regexNameValidation() {
-        String validation = "(?i)(^[a-záéíóúüñç])((?![ .,'-]$)[a-záéíóúüñç .,'-]){0,24}$";
-        return validation;
-    }
-
-    public Boolean setName(String name) {
-        if(name.matches(regexNameValidation())) {
-            this.name = name;
-            return true;
-        } else {
-            throw new IllegalArgumentException("Name format should be valid.");
-        }
-    }
-
-    public List<Lead> getLeads() {
-        return leads;
-    }
-
-    public void setLeads(List<Lead> leads) {
-        this.leads = leads;
-    }
-
-    public void addLead(Lead lead){
-        leads.add(lead);
-    }
-
-    public List<Opportunity> getOpportunities() {
-        return opportunities;
-    }
-
-    public void addOpportunity(Opportunity opportunity){
-        opportunities.add(opportunity);
-    }
-
-    public void setOpportunities(List<Opportunity> opportunities) {
-        this.opportunities = opportunities;
-    }
-
-    public String toStringFormatted() {
-        return  "ID " + id +
-                " | Name: " + name;
-    }
-
-    public static void wrongIdSalesRep() {
-        System.out.println(Colors.RESET+Colors.LIGHT_RED+ "Insert a SalesRep id bigger than zero");
+    public void setName(String name) {
+        this.name = name;
     }
 }
